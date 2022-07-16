522fd537d6dd2d6fe77025847ab3a7954d7b8d56
ValdemirRodrigues <valde_crodrigues@hotmail.com>
2022-07-16 12:48:15 -0300

    Transformando em função
    Otimizando o Código tudo def (função)

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-# View
-usuario_digitado = input("Informe seu usuário: ")
-senha_digitado = input("Informe seu usuário: ")
+# View - Tela visual
+def view():
+    usuario_digitado = input("Informe seu usuário: ")
+    senha_digitado = input("Informe seu usuário: ")
 
 # Model - O que vem no Banco 
-usuario_BD ='joao'
-senha_BD = '123'
+def model():
+    usuario_BD ='joao'
+    senha_BD = '123'
 
-# Controle
-if usuario_digitado == usuario_BD and senha_digitado == senha_BD:
-    print ("pode entrar")
-else:
-    print ("usuario ou senha invalido")
+# Controle - A validação
+def controller():
+    if usuario_digitado == usuario_BD and senha_digitado == senha_BD:
+        print ("pode entrar")
+    else:
+        print ("usuario ou senha invalido")
