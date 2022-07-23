5fddf25c1684d7c1c8f788821fc81e63d229663f
ValdemirRodrigues <valde_crodrigues@hotmail.com>
2022-07-23 12:57:55 -0300

    Organização
    Otimizando

--- controllers/controllerAplicacao.py
+++ controllers/controllerAplicacao.py
@@ -1,14 +1,16 @@
 import models.banco as banco
 import views.formulario as view
 # Controle - A validação
-def validar_login(usuario_digitado, senha_digitado):
+def validar_login(usuario_completo):
     usuario_BD = banco.model_usuario()
     senha_BD = banco.model_senha()
     
-    if usuario_digitado == usuario_BD and senha_digitado == senha_BD:
+    if usuario_completo[0]== usuario_BD and usuario_completo[1] == senha_BD:
         print ("pode entrar")
     else:
         print ("usuario ou senha invalido")
 
 def iniciar():
-    view.formulario_login()
+    usuario_completo = view.formulario_login()
+    validar_login(usuario_completo)
+    #validar_login(viw.formulario_login())
--- views/formulario.py
+++ views/formulario.py
@@ -1,6 +1,13 @@
-import controllers.controllerAplicacao as controllerAplicacao
+
 # View - Tela visual - o que vai para o usuário
 def formulario_login():
     usuario_digitado = input("Informe seu usuário: ")
     senha_digitado = input("Informe seu usuário: ")
-    controllerAplicacao.validar_login(usuario_digitado, senha_digitado)
+    usuario_completo = [usuario_digitado, senha_digitado]
+    return usuario_completo
+
+def exibir_mensagem(texto)
+    print("\n\n\n\")
+    print("====================")
+    print("\n\n\n\")
+    print("====================")    
