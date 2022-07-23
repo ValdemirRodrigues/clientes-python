9e178e9706f23825c47d51c7a6bd0cb995cf9796
ValdemirRodrigues <valde_crodrigues@hotmail.com>
2022-07-23 13:39:20 -0300

    menu
    Chamando menu

--- controllers/controllerAplicacao.py
+++ controllers/controllerAplicacao.py
@@ -6,11 +6,22 @@ def validar_login(usuario_completo):
     senha_BD = banco.model_senha()
     
     if usuario_completo[0]== usuario_BD and usuario_completo[1] == senha_BD:
-        print ("pode entrar")
+        view.exibir_mensagem("pode entrar")
+        opcoes_menu()
     else:
-        print ("usuario ou senha invalido")
-
+        view.exibir_mensagem ("usuario ou senha invalido")
+        iniciar()
 def iniciar():
     usuario_completo = view.formulario_login()
     validar_login(usuario_completo)
     #validar_login(viw.formulario_login())
+
+def opcoes_menu():
+    opcao = view.menu()
+    if opcao == '1':
+        print("Cadastro de clientes")     
+    elif opcao == '2':
+        print("Listagem de clientes")     
+    else:
+        view.exibir_mensagem("Sistema finalizado")   
+        exit()
--- views/formulario.py
+++ views/formulario.py
@@ -6,8 +6,16 @@ def formulario_login():
     usuario_completo = [usuario_digitado, senha_digitado]
     return usuario_completo
 
-def exibir_mensagem(texto)
-    print("\n\n\n\")
+def exibir_mensagem(texto):
+    print("\n\n\n")
     print("====================")
-    print("\n\n\n\")
+    print(texto)
+    print("\n\n\n")
     print("====================")    
+
+def menu():
+    print(" 1 - Para cadastrar novo cliente")
+    print(" 2 - Para listar todos os cliente")
+    print(" 3 - Para sair")
+    opcao = input("Digite a opção")
+    return opcao
